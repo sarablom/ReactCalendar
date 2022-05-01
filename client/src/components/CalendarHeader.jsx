c8d747e47165062e4a6d111cf7080b7782b5500d
Sara Blom <sara.cecilia.blom@gmail.com>
2022-05-01 14:50:39 +0200

    style calender header component

--- client/src/components/CalendarHeader.jsx
+++ client/src/components/CalendarHeader.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 export const CalendarHeader = ({ onNext, onBack, dateDisplay }) => {
-  return(
-    <div id="header">
+  return (
+    <HeaderWrapper id="header">
       <div id="monthDisplay">{dateDisplay}</div>
       <div>
-        <button onClick={onBack} id="backButton">Föregående</button>
-        <button onClick={onNext} id="nextButton">Nästa</button>
+        <button onClick={onBack} id="backButton">
+          Föregående
+        </button>
+        <button onClick={onNext} id="nextButton">
+          Nästa
+        </button>
       </div>
-    </div>
+    </HeaderWrapper>
   );
 };
+
+const HeaderWrapper = styled.header `
+  padding: 10px;
+  color: #d36c6c;
+  font-size: 26px;
+  font-family: sans-serif;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+  background-color:#92a1d1;
+}
+`
