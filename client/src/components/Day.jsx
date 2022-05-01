a6d377be7c54c9a2f7e23c9a11facaf4d1fdf6ae
Sara Blom <sara.cecilia.blom@gmail.com>
2022-05-01 14:56:36 +0200

    style day component

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 export const Day = ({ day, onClick }) => {
-  const className = `day ${day.value === 'padding' ? 'padding' : ''} ${day.isCurrentDay ? 'currentDay' : ''}`;
-  
-  return (
-    <div onClick={onClick} className={className}>
-      {day.value === 'padding' ? '' : day.value}
+  const className = `day ${day.value === "padding" ? "padding" : ""} ${
+    day.isCurrentDay ? "currentDay" : ""
+  }`;
 
-      {day.event && <div className='event'>{day.event.title}</div>}
-    </div>
+  return (
+    <DayWrapper onClick={onClick} className={className}>
+      {day.value === "padding" ? "" : day.value}
+
+      {day.event && (
+        <EventWrapper className="event">{day.event.title}</EventWrapper>
+      )}
+    </DayWrapper>
   );
 };
+
+const DayWrapper = styled.div`
+  cursor: default !important;
+  background-color: #fffcff !important;
+  box-shadow: none !important;
+`;
+
+const EventWrapper = styled.div`
+  font-size: 10px;
+  padding: 3px;
+  background-color: #58bae4;
+  color: white;
+  border-radius: 5px;
+  max-height: 55px;
+  overflow: hidden;
+`;
